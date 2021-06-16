df54f05ecf5bf951c7c0810873eaed9305da034e
Louis Fredon <fredonlouis99@gmail.com>
2021-06-16 12:10:27 +0200

    changements des mots

--- R/data_fixture.R
+++ R/data_fixture.R
@@ -11,78 +11,52 @@
 data_fixture <- function(){
 
 
-  library(quanteda)
-  library(magrittr)
-  ########## Creation du DFM
+  x<-data.frame()
 
-
-  #On crée un data avec les politiciens, ici on prends des mots des présidents américains lors de leurs discours
-
-  data_pol <- data_corpus_inaugural %>% corpus_subset(Year > 2011) %>%tokens()
-  data_pol  <-dfm(data_pol)# on transforme en dfm
-  data_pol <- dfm_keep(data_pol, min_nchar = 2)#on garde que les mots de plus de 2 lettres
-  data_pol <- dfm_remove(data_pol, pattern = stopwords("en"))#on enleve les mots communs (ponctuation, et ..)
-  data_pol <- convert(data_pol, to = "data.frame")
-  rownames(data_pol) <- data_pol[,1]
-  data_pol <- data_pol[,-1]
-
-#word1 a la place
+  for (i in 1:100){
+    x[1,i]<-paste0("words",i)
+  }
 
   # On crée un data users avec des occurences aléatoires
 
-  a<-c((1:50)) # colonnes des users
+  a<-data.frame() # colonnes des users
 
+  for (i in 1:50){
+    a[i,1]<-paste0("users",i)
+  }
 
   b<-c(rpois(50, 0.5)) # occurences
 
   data_users <- data.frame(a,b)
-  data_users <- data_users[,-1]
   data_users <- as.data.frame(data_users)
 
-  for(i in 2:dim(data_pol)[2]){
+  for(i in 3:101){
     data_users[,i] <- round(abs(c(rpois(50, 0.5)))) # on rajoute autant qu'il y a de mots
   }
 
-  colnames(data_users) <- colnames(data_pol) # on mets les mêmes noms pour fusionner
+  rownames(data_users)<-data_users[,1]
 
-  data <- rbind(data_pol, data_users)
+  data_users<-data_users[,2:101]
 
-  data[,dim(data)[2]+1] <- 1
-
-
-
-  for(i in 1:dim(data)[1]){
-    if(i<4){
-      data[i,dim(data)[2]]=1
-    }
-    else{data[i,dim(data)[2]]=0}
-  }
-
-  data[,dim(data)[2]]
-
-  colnames(data)[dim(data)[2]] <- "ID"
+  colnames(data_users) <- x # on mets les mêmes noms pour fusionner
 
 
-  data <- data[4:53,1:100]
-
-  dfm_fixture <- data# data finale
+  dfm_fixture <- data_users# data finale
 
   remove(a)
   remove(b)
   remove(i)
   remove(data_users)
-  remove(data_pol)
-  remove(data)
+  remove(x)
 
 
 
-
-  ########### Creation du Data frame validation
+########### Creation du Data frame validation
 
   df_validation <- data.frame(rownames(dfm_fixture))
   colnames(df_validation) <- "users_id"
-  for(i in 1:dim(df_validation)[1]){
-    df_validation[i,2] <- rnorm(10, mean=5, sd=3)
+  for(i in 1:50){
+    df_validation[i,2] <- rnorm(1, mean=0, sd=1)
   }
 remove(i)
 
