036fedbbb1a70192e5594ae46c86e514fd2ee688
Louis Fredon <fredonlouis99@gmail.com>
2021-07-15 14:14:31 +0200

    .R test sur donnees reelles

--- test_donnees_reelles.R
+++ test_donnees_reelles.R
@@ -1,4 +1,10 @@
-### algo _10 sur les données réelles
+
+# Algo _10 sur les données réelles ---------------
 
 library(DLA)
-algo_10_review(TM2gram_test_without_na,op_without_na,"Vpl_ID")
+
+set.seed(42)
+
+x <- algo_10_review(TM2gram_test_without_na,
+                    op_without_na,
+                    "Vpl_ID")
